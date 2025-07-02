1a139209ca5d315a9c4f76fe49f591d0395af66a
Мягмарсүрэн Сүхбаатар [IC] <myagmarsuren.s@invescore.mn>
2025-07-02 19:18:17 +0800

    app.py port tohirgoo nemsen

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 from flask import Flask, render_template, request, redirect, url_for
 import random
+import os
+
+port = int(os.environ.get('PORT', 5000))
+
+app.run(host='0.0.0.0', port=port)
+
 
 app = Flask(__name__)
 
