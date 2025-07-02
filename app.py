e2eca4aba4bacf2434fdebdbc210fcef268f1d42
Мягмарсүрэн Сүхбаатар [IC] <myagmarsuren.s@invescore.mn>
2025-07-02 19:26:28 +0800

    app.py port tohirgoo nemsen

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ import os
 
 port = int(os.environ.get('PORT', 5000))
 
-app.run(host='0.0.0.0', port=port)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=port)
 
 
 app = Flask(__name__)
